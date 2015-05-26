cf94abee45054dcf510a99b69176a937e0348034
Waltton <waltton@outlook.com>
2015-05-26 00:17:09 -0300

    alteração para ajustar a função defineProximoJ...

--- executarJogada.m
+++ executarJogada.m
@@ -3,6 +3,7 @@ function [ h ] = executarJogada( handles, linha, coluna )
 if jogadaEhPermitida(handles, linha, coluna)
     handles.posicoes(linha, coluna) = handles.jogadorAtual;
     atualizarGUI(handles);
+    handles.jogadorAtual = defineProximoJogador(handles.jogadorAtual);
 else
     disp('jogada não permitida...')
 end
