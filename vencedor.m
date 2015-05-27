3c2274fa493f1e190a096cd497881cfacc36c187
Waltton <waltton@outlook.com>
2015-05-27 00:01:39 -0300

    corrigindo erro
    indiferente da combinação vencedora retornava o valor de p(1, 1)

--- vencedor.m
+++ vencedor.m
@@ -7,10 +7,10 @@ if ((p(1, 1) ~= 0) && p(1, 1) == p(1, 2) &&  p(1, 1) == p(1, 3))
     jogador = p(1, 1);
 end
 if ((p(2, 1) ~= 0) && p(2, 1) == p(2, 2) &&  p(2, 1) == p(2, 3)) 
-    jogador = p(1, 1);
+    jogador = p(2, 1);
 end
 if ((p(3, 1) ~= 0) && p(3, 1) == p(3, 2) &&  p(3, 1) == p(3, 3)) 
-    jogador = p(1, 1);
+    jogador = p(3, 1);
 end
 
 %verticais
@@ -18,10 +18,10 @@ if ((p(1, 1) ~= 0) && p(1, 1) == p(2, 1) &&  p(1, 1) == p(3, 1))
     jogador = p(1, 1);
 end
 if ((p(1, 2) ~= 0) && p(1, 2) == p(2, 2) &&  p(1, 2) == p(3, 2)) 
-    jogador = p(1, 1);
+    jogador = p(1, 2);
 end
 if ((p(1, 3) ~= 0) && p(1, 3) == p(2, 3) &&  p(1, 3) == p(3, 3)) 
-    jogador = p(1, 1);
+    jogador = p(1, 3);
 end
 
 %diagonais
@@ -29,7 +29,7 @@ if ((p(1, 1) ~= 0) && p(1, 1) == p(2, 2) &&  p(1, 1) == p(3, 3))
     jogador = p(1, 1);
 end
 if ((p(1, 3) ~= 0) && p(1, 3) == p(2, 2) &&  p(1, 3) == p(3, 1)) 
-    jogador = p(1, 1);
+    jogador = p(1, 3);
 end
 
 
