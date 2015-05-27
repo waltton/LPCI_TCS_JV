502cedd6458e792cb529bcf6433b97deddee6b22
Waltton <waltton@outlook.com>
2015-05-27 00:17:25 -0300

    alteração na msg
    exibe msg de jogada não permitida apenas quando o jogo está ativo.

--- executarJogada.m
+++ executarJogada.m
@@ -9,9 +9,11 @@ if jogadaEhPermitida(handles, linha, coluna)
         set(handles.text2, 'String', sprintf('Jogador atual : jogador %d', handles.jogadorAtual));
     end
 else
-    set(handles.text2, 'String', 'Jogada não permitida!');
-    t = timer('TimerFcn',@(x,y)set(handles.text2, 'String', sprintf('Jogador atual : jogador %d', handles.jogadorAtual)),'StartDelay',3);
-    start(t);
+    if handles.jogoAtivo
+        set(handles.text2, 'String', 'Jogada não permitida!');
+        t = timer('TimerFcn',@(x,y)set(handles.text2, 'String', sprintf('Jogador atual : jogador %d', handles.jogadorAtual)),'StartDelay',3);
+        start(t);
+    end
 end
 
 h = handles;
