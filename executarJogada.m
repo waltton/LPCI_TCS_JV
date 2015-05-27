864998d28bb12ab973a43aeb1be15ab39bdc2e75
Waltton <waltton@outlook.com>
2015-05-27 00:15:16 -0300

    alterações nas mensagens
    exibe a mensagem de jogada proibida por apenas 3 segundos

--- executarJogada.m
+++ executarJogada.m
@@ -1,14 +1,17 @@
 function [ h ] = executarJogada( handles, linha, coluna )
-
-set(handles.text2, 'String', '...');
     
 if jogadaEhPermitida(handles, linha, coluna)
     handles.posicoes(linha, coluna) = handles.jogadorAtual;
     handles.jogoAtivo = verificaSituacaoDoJogo(handles);
     atualizarGUI(handles);
     handles.jogadorAtual = proximoJogador(handles.jogadorAtual);
+    if handles.jogoAtivo
+        set(handles.text2, 'String', sprintf('Jogador atual : jogador %d', handles.jogadorAtual));
+    end
 else
     set(handles.text2, 'String', 'Jogada não permitida!');
+    t = timer('TimerFcn',@(x,y)set(handles.text2, 'String', sprintf('Jogador atual : jogador %d', handles.jogadorAtual)),'StartDelay',3);
+    start(t);
 end
 
 h = handles;
