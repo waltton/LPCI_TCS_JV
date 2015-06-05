f94a28c0fbbfd1c934d17d594d221089b7e2d760
Waltton <waltton@outlook.com>
2015-06-05 10:57:38 -0300

    usando XO no gui

--- atualizarGUI.m
+++ atualizarGUI.m
@@ -1,15 +1,15 @@
 function [ ] = atualizarGUI( handles )
-set(handles.pushbutton2, 'String', handles.posicoes(1, 1));
-set(handles.pushbutton3, 'String', handles.posicoes(1, 2));
-set(handles.pushbutton4, 'String', handles.posicoes(1, 3));
+set(handles.pushbutton2, 'String', num2XO(handles.posicoes(1, 1)));
+set(handles.pushbutton3, 'String', num2XO(handles.posicoes(1, 2)));
+set(handles.pushbutton4, 'String', num2XO(handles.posicoes(1, 3)));
 
-set(handles.pushbutton5, 'String', handles.posicoes(2, 1));
-set(handles.pushbutton6, 'String', handles.posicoes(2, 2));
-set(handles.pushbutton7, 'String', handles.posicoes(2, 3));
+set(handles.pushbutton5, 'String', num2XO(handles.posicoes(2, 1)));
+set(handles.pushbutton6, 'String', num2XO(handles.posicoes(2, 2)));
+set(handles.pushbutton7, 'String', num2XO(handles.posicoes(2, 3)));
 
-set(handles.pushbutton8, 'String', handles.posicoes(3, 1));
-set(handles.pushbutton9, 'String', handles.posicoes(3, 2));
-set(handles.pushbutton10, 'String', handles.posicoes(3, 3))
+set(handles.pushbutton8, 'String', num2XO(handles.posicoes(3, 1)));
+set(handles.pushbutton9, 'String', num2XO(handles.posicoes(3, 2)));
+set(handles.pushbutton10, 'String', num2XO(handles.posicoes(3, 3)));
 
 set(handles.pushbutton2, 'Enable', boolToOnOff(handles.jogoAtivo && handles.posicoes(1, 1) == 0));
 set(handles.pushbutton3, 'Enable', boolToOnOff(handles.jogoAtivo && handles.posicoes(1, 2) == 0));
